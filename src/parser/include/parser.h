808dec02912375ffe825be7cd5dc47cb3543a9d2
MarcosCasiraghi <mcasiraghi@itba.edu.ar>
2023-06-02 09:29:33 -0300

    feat: added input parser

---
+++ src/parser/include/parser.h
@@ -0,0 +1,67 @@
+#ifndef COMMAND_PARSER_H
+#define COMMAND_PARSER_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "../../utils/include/buffer.h"
+
+#define MAX_MSG_SIZE 512
+#define MAX_ARG_LENGTH 40
+#define ALL_CMD_SIZE  9
+
+typedef enum command_type {
+    CMD_NOT_RECOGNIZED     = -1,
+    CMD_USER               =  0,
+    CMD_PASS               =  1,
+    CMD_RETR               =  2,
+    CMD_LIST               =  3,
+    CMD_CAPA               =  4,
+    CMD_QUIT               =  5,
+    CMD_DELE               =  6,
+    CMD_NOOP               =  7,
+    CMD_STAT               =  8,
+} command_type;
+
+typedef struct command_instance {
+    command_type    type;
+    char *       argument;
+    bool         is_multi;
+} command_instance;
+
+typedef enum command_state {
+    COMMAND_TYPE,
+    COMMAND_ARGS,
+    COMMAND_CRLF,
+    COMMAND_ERROR,
+} command_state;
+
+typedef struct input_parser {
+    int                 line_size;
+    int                 crlf_state;  //0 NONE, 1 \r READ, 2 \n READ
+    int                 arg_length;
+    int                 args_count;
+    int                 invalid_size;
+    bool                invalid_type[ALL_CMD_SIZE];     //completa arreglo a medida que comando no coincida
+    command_state       state;
+    command_instance    current_command;
+    bool                is_expecting_new_arg;
+} input_parser;
+
+/**
+ * Inicializa el parser
+ */
+void parser_init(input_parser * parser);
+
+/**
+ * Entrega un char al parser. Deja a finished en true si se llego al final
+ */
+command_state parser_feed(input_parser * parser, const char c, bool * finished);
+
+/**
+ * Consume chars de un buffer - llama a parser feed en un ciclo
+ * Termina cuando se consumio todo
+ */
+command_state parser_consume(input_parser * parser, buffer* buffer, bool * finished,size_t * n_consumed);
+
+#endif
---
+++ src/parser/parser.c
@@ -0,0 +1,235 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "./include/parser.h"
+#include "./logger.h"
+
+#define IS_MULTILINE(command, args_count) (command->type == CMD_CAPA       \
+                ||  (command->type == CMD_LIST && (args_count == 0))         \
+                ||  (command->type == CMD_RETR && (args_count == 1))         \
+                )
+
+typedef struct command_info {
+    command_type type;
+    char *    name;
+    int       len;
+    int       min_args;
+    int       max_args;
+} command_info;
+
+static const command_info all_command_info[] = {
+        {
+                .type = CMD_USER, .name = "USER", .len = 4, .min_args = 1, .max_args = 1,
+        } , {
+                .type = CMD_PASS, .name = "PASS", .len = 4, .min_args = 1, .max_args = 1,
+        } , {
+                .type = CMD_RETR, .name = "RETR", .len = 4, .min_args = 1, .max_args = 1,
+        } , {
+                .type = CMD_LIST, .name = "LIST", .len = 4, .min_args = 0, .max_args = 1,
+        } , {
+                .type = CMD_CAPA, .name = "CAPA", .len = 4, .min_args = 0, .max_args = 0,
+        } , {
+                .type = CMD_QUIT, .name = "QUIT", .len = 4, .min_args = 0, .max_args = 0,
+        },  {
+                .type = CMD_DELE, .name = "DELE", .len = 4, .min_args = 1, .max_args = 1,
+        },  {
+                .type = CMD_NOOP, .name = "NOOP", .len = 4, .min_args = 0, .max_args = 0,
+        },  {
+                .type = CMD_STAT, .name = "STAT", .len = 4, .min_args = 0, .max_args = 0,
+        }
+};
+
+static void command_recognition (input_parser * parser, char c, bool * finished, command_instance * current_command);
+static void with_arguments_state (input_parser * parser, char c, bool * finished, command_instance * current_command);
+static void crlf_state (input_parser * parser, char c, bool * finished, command_instance * current_command);
+static void error_state (input_parser * parser, char c, bool * finished, command_instance * current_command);
+
+
+static void handle_parsed(command_instance * current_command, input_parser * parser, bool * finished, bool not_match);
+
+void parser_init(input_parser * parser) {
+    parser->state          = COMMAND_TYPE;
+    parser->line_size       = 0;
+    parser->arg_length      = 0;
+    parser->args_count      = 0;
+    parser->is_expecting_new_arg = false;
+}
+
+command_state parser_feed(input_parser * parser, const char c, bool * finished) {
+    command_instance * current_command = &parser->current_command;
+
+    if(parser->line_size == 0) {
+        current_command->type = CMD_NOT_RECOGNIZED;
+        current_command->argument = NULL;
+        parser->crlf_state   = 0;
+        parser->args_count    = 0;
+        parser->invalid_size = 0;
+        for(int i = 0; i < ALL_CMD_SIZE; i++)
+            parser->invalid_type[i] = false;
+    }
+
+    if (parser->state > COMMAND_ERROR){ // Nunca deberia entrar en este caso pero se deja por claridad
+        log(ERROR,"Command parser not reconize state: %d", parser->state);
+    }
+    else{
+        if(parser->state == COMMAND_TYPE ){
+            command_recognition(parser, c, finished, current_command);
+        }else if( parser->state == COMMAND_ARGS){
+            with_arguments_state(parser, c, finished, current_command);
+        }else if( parser->state == COMMAND_CRLF){
+            crlf_state(parser, c, finished, current_command);
+        }else{  //type == COMMAND_ERROR
+            error_state(parser, c, finished, current_command);
+        }
+    }
+
+    if(parser->line_size++ == MAX_MSG_SIZE || (parser->state == COMMAND_ARGS && parser->arg_length > MAX_ARG_LENGTH)){
+        parser->state = COMMAND_ERROR;
+        printf("entre aca\n");
+    }
+    return parser->state;
+}
+
+command_state parser_consume(input_parser * parser, buffer* buffer, bool * finished, size_t * n_consumed) {
+    command_state state = parser->state;
+    size_t n = 0;
+    while(buffer_can_read(buffer)) {
+        n++;
+        const uint8_t c = buffer_read(buffer);
+        state = parser_feed(parser, c, finished);
+        if(*finished) {
+            break;
+        }
+    }
+    *n_consumed = n;
+    return state;
+}
+
+
+static void handle_parsed(command_instance * current_command, input_parser * parser, bool * finished, bool not_match) {
+
+    if(not_match) {
+        current_command->type = CMD_NOT_RECOGNIZED;
+        if(current_command->argument != NULL) {
+            free(current_command->argument);
+            current_command->argument = NULL;
+        }
+    }
+
+    current_command->is_multi = IS_MULTILINE(current_command, parser->args_count);
+
+    parser->state     = COMMAND_TYPE;
+    parser->line_size  = -1;
+    parser->arg_length--;
+    *finished = true;
+
+}
+
+// modules for parser_feed's switch
+static void command_recognition (input_parser * parser, const char c, bool * finished, command_instance * current_command) {
+    if(c != '\n') {
+        for(int i = 0; i < ALL_CMD_SIZE; i++) {
+            if(!parser->invalid_type[i]) {
+                if(toupper(c) != all_command_info[i].name[parser->line_size]) {
+                    parser->invalid_type[i] = true;
+                    parser->invalid_size++;
+                } else if(parser->line_size == all_command_info[i].len-1) {
+                    current_command->type = all_command_info[i].type;
+                    parser->arg_length = 0;
+                    if(all_command_info[i].max_args > 0) {
+                        if (current_command->argument == NULL) {
+                            current_command->argument = calloc(MAX_ARG_LENGTH + 1,sizeof(char));    //NULL TERMINATED
+                        }
+                        else{
+                            memset(current_command->argument,0,MAX_ARG_LENGTH + 1);
+                        }
+                        parser->state = COMMAND_ARGS;
+                    } else
+                        parser->state = COMMAND_CRLF;
+                    break;
+                }
+            }
+            if(parser->invalid_size == ALL_CMD_SIZE)
+                parser->state = COMMAND_ERROR;
+        }
+    } else
+        handle_parsed(current_command, parser, finished, true);
+}
+
+static void with_arguments_state (input_parser * parser, const char c, bool * finished, command_instance * current_command) {
+
+    // Espacio indica que deberia venir argumento
+    if(c == ' ') {
+        parser->is_expecting_new_arg = true;
+    }
+    // Leyendo un argumento
+    else if(c != '\r' && c != '\n') {
+        if (parser->is_expecting_new_arg){
+            if(parser->args_count >= all_command_info[current_command->type].max_args)
+                parser->state = COMMAND_ERROR;
+            else if(parser->arg_length == 0)
+                parser->arg_length++;
+            // else if(parser->arg_length > 1 /*&& parser->args_count < all_command_info[current_command->type].max_args*/) {
+            //     // parser->arg_length = 1;
+            //     parser->args_count++;
+            // }
+            parser->args_count++;
+            parser->is_expecting_new_arg = false;
+        }
+        parser->crlf_state = 0;
+        if(parser->arg_length == 0)
+            parser->state = COMMAND_ERROR;
+        else {
+            if(all_command_info[parser->current_command.type].max_args > 0)
+                (current_command->argument)[parser->arg_length-1] = c;
+                
+            parser->arg_length++;
+        }
+    }
+    else if(c == '\r') {
+        parser->crlf_state = 1;
+        if(all_command_info[parser->current_command.type].max_args > 0)
+            (current_command->argument)[parser->arg_length > 0 ? parser->arg_length-1: 0] = 0;     //username null terminated
+        if( parser->args_count >= all_command_info[current_command->type].min_args && parser->args_count <= all_command_info[current_command->type].max_args){
+            parser->state = COMMAND_CRLF;
+        } else{
+            parser->state = COMMAND_ERROR;
+        }
+    }
+    // Es imposible que c != '\n' si llegamos aca, es un tema de claridad
+    else if(c == '\n' && parser->crlf_state == 1) {
+        parser->crlf_state = 2;
+        if(all_command_info[parser->current_command.type].max_args > 0)
+            (current_command->argument)[parser->arg_length-1] = 0;     //username null terminated
+        if(all_command_info[current_command->type].min_args <= parser->args_count && parser->args_count <= all_command_info[current_command->type].max_args) {
+            handle_parsed(current_command, parser, finished, false);
+        } else
+            handle_parsed(current_command, parser, finished, true);
+    } else {
+        parser->crlf_state = 0;
+        parser->state = COMMAND_ERROR;
+    }
+}
+
+static void crlf_state (input_parser * parser, const char c, bool * finished, command_instance * current_command) {
+    if(c == '\r' && parser->crlf_state == 0) {
+        parser->crlf_state = 1;
+    } else if(c == '\n' && parser->crlf_state == 1){
+        handle_parsed(current_command, parser, finished, false);
+    } else {
+        if( c != ' ')
+            parser->state = COMMAND_ERROR;
+    }
+}
+
+static void error_state (input_parser * parser, const char c, bool * finished, command_instance * current_command) {
+    if(c == '\r' && parser->crlf_state == 0) {
+        parser->crlf_state = 1;
+    } else if(c == '\n'){
+        handle_parsed(current_command, parser, finished, true);
+    }else{
+        parser->crlf_state = 0;
+    }
+}
