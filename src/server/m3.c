05ee40d0c4aa63dbc1690424dd7b2718b6a3e056
MSGronda <mgronda@itba.edu.ar>
2023-06-23 15:09:34 -0300

    refactor: sizeof constant in m3

--- src/server/m3.c
+++ src/server/m3.c
@@ -18,19 +18,19 @@ void mng_buffer_to_request(uint8_t * request, mng_request * converted) {
 
     // EXP: al ser de solo 1 byte, no hace falta la conversion
     converted->version = GET_UINT8(request);
-    offset += sizeof(converted->version);
+    offset += sizeof(uint8_t);
 
     converted->auth_token = GET_UINT32(request + offset);
-    offset += sizeof(converted->auth_token);
+    offset += sizeof(uint32_t);
 
     converted->op_code = GET_UINT8(request + offset);
-    offset += sizeof(converted->op_code);
+    offset += sizeof(uint8_t);
 
     converted->request_id = GET_UINT8(request + offset);
-    offset += sizeof(converted->request_id);
+    offset += sizeof(uint8_t);
 
     size_t len = GET_UINT16(request + offset);
-    offset += sizeof(converted->length);
+    offset += sizeof(uint16_t);
 
     // EXP: me protejo en contra de que me mande mas de lo permitido
     converted->length =  len > MAX_DATA_LEN ? MAX_DATA_LEN : len;
@@ -42,21 +42,21 @@ void mng_response_to_buffer(mng_response * response, uint8_t * converted, size_t
 
     size_t offset = 0;
 
-    memcpy(converted + offset, &response->version, sizeof(response->version));
-    offset += sizeof(response->version);
+    memcpy(converted + offset, &response->version, sizeof(uint8_t));
+    offset += sizeof(uint8_t);
 
-    memcpy(converted + offset, &response->status, sizeof(response->status));
-    offset += sizeof(response->status);
+    memcpy(converted + offset, &response->status, sizeof(uint8_t));
+    offset += sizeof(uint8_t);
 
-    memcpy(converted + offset, &response->op_code, sizeof(response->op_code));
-    offset += sizeof(response->op_code);
+    memcpy(converted + offset, &response->op_code, sizeof(uint8_t));
+    offset += sizeof(uint8_t);
 
-    memcpy(converted + offset, &response->request_id, sizeof(response->request_id));
-    offset += sizeof(response->request_id);
+    memcpy(converted + offset, &response->request_id, sizeof(uint8_t));
+    offset += sizeof(uint8_t);
 
     uint16_t length = response->length;
-    memcpy(converted + offset, &length, sizeof(response->length));
-    offset += sizeof(length);
+    memcpy(converted + offset, &length, sizeof(uint16_t));
+    offset += sizeof(uint16_t);
 
     memcpy(converted + offset, response->data, response->length);
     offset += response->length;
@@ -69,16 +69,16 @@ void mng_buffer_to_response(uint8_t * request, mng_response * converted) {
 
     // EXP: al ser de solo 1 byte, no hace falta la conversion
     converted->version = GET_UINT8(request);
-    offset += sizeof(converted->version);
+    offset += sizeof(uint8_t);
 
     converted->status = GET_UINT8(request + offset);
-    offset += sizeof(converted->status);
+    offset += sizeof(uint8_t);
 
     converted->op_code = GET_UINT8(request + offset);
-    offset += sizeof(converted->op_code);
+    offset += sizeof(uint8_t);
 
     converted->request_id = GET_UINT8(request + offset);
-    offset += sizeof(converted->request_id);
+    offset += sizeof(uint8_t);
 
     uint16_t len = GET_UINT16(request + offset);
     offset += sizeof(uint16_t);
@@ -92,22 +92,22 @@ void mng_buffer_to_response(uint8_t * request, mng_response * converted) {
 void mng_request_to_buffer(mng_request * request, uint8_t * converted, size_t * converted_len) {
     size_t offset = 0;
 
-    memcpy(converted + offset, &request->version, sizeof(request->version));
-    offset += sizeof(request->version);
+    memcpy(converted + offset, &request->version, sizeof(uint8_t));
+    offset += sizeof(uint8_t);
 
     uint32_t token = request->auth_token;    
-    memcpy(converted + offset, &token, sizeof(token));
+    memcpy(converted + offset, &token, sizeof(uint32_t));
     offset += sizeof(uint32_t);
 
-    memcpy(converted + offset, &request->op_code, sizeof(request->op_code));
-    offset += sizeof(request->op_code);
+    memcpy(converted + offset, &request->op_code, sizeof(uint8_t));
+    offset += sizeof(uint8_t);
 
-    memcpy(converted + offset, &request->request_id, sizeof(request->request_id));
-    offset += sizeof(request->request_id);
+    memcpy(converted + offset, &request->request_id, sizeof(uint8_t));
+    offset += sizeof(uint8_t);
 
     uint16_t len = request->length;
     memcpy(converted + offset, &len, sizeof(uint16_t));
-    offset += sizeof(request->length);
+    offset += sizeof(uint16_t);
 
     memcpy(converted + offset, &request->data, request->length);
     offset += request->length;
